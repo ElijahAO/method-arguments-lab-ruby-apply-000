0587d61b6d2b4ba2b79bf04fc9293ffe73128bfd
Elijah Adebiyi Oyeniyi <biyibama@gmail.com>
2019-01-09 13:02:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,4 @@
 # lib/introduction.rb
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
-end
-
-def introduction_with_language(name , language)
-  puts("Hi, my name is #{name} and i am learning to program in #{language}.")
-end
+def introduction
+  
