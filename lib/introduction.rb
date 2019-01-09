fe7f0c2814aa94273e84850626bfc26d26beaaf6
Elijah Adebiyi Oyeniyi <biyibama@gmail.com>
2019-01-09 13:43:25 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+# lib/introduction.rb
+
+def introduction(name)
+  puts "Hi, my name is #{'Josh'}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{'Dan'} and I am learning to program in #{'Ember.js'}."
+end
